9f92319f9bffedaeebbb5502dac66fbaceb2e694
m3500 <mukhadis.yaru@gmail.com>
2025-09-02 15:38:49 +0100

    Configured ECR policy

--- Terraform/ecr.tf
+++ Terraform/ecr.tf
@@ -1,8 +1,37 @@
 resource "aws_ecr_repository" "repo" {
-  name                 = "backend-repo"
-  image_tag_mutability = "MUTABLE"
+  name = "backend-repo"
+}
 
-  image_scanning_configuration {
-    scan_on_push = true
+data "aws_iam_policy_document" "ecr_policy" {
+  statement {
+    sid    = "new policy"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
   }
+}
+
+resource "aws_ecr_repository_policy" "repo" {
+  repository = aws_ecr_repository.repo.name
+  policy     = data.aws_iam_policy_document.ecr_policy.json
 }
