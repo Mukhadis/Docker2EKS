f5b680d84bcbc4b0698e3ea03472f261d0ac7a52
m3500 <mukhadis.yaru@gmail.com>
2025-09-03 11:34:48 +0100

    EKS cluster fix, Added managed node group and IAM roles to access ECR. k8s manifest updated and region changes to us-east-1

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -7,6 +7,7 @@ module "eks" {
 
   endpoint_public_access = true
 
+  # EKS Auto Mode configuration
   compute_config = {
     enabled    = true
     node_pools = ["general-purpose"]
@@ -15,8 +16,40 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
+  # Create IAM service role for EKS cluster
+  iam_role_name = "${local.name}-eks-cluster-role"
+  iam_role_additional_policies = {
+    AmazonEKSClusterPolicy = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  }
+
   tags = {
     Name : "DevOpsProject0-EKS"
     CreationDate : "${formatdate("YYYY-MM-DD", timestamp())}"
   }
 }
+
+# IAM role policy attachment for ECR access
+resource "aws_iam_role_policy_attachment" "eks_ecr_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = module.eks.cluster_iam_role_name
+}
+
+# Additional IAM policy for ECR token authorization (if needed for cross-account access)
+resource "aws_iam_role_policy" "eks_ecr_token_policy" {
+  name = "${local.name}-eks-ecr-token-policy"
+  role = module.eks.cluster_iam_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,9 @@
 locals {
   name   = "my-web-app"
-  region = "eu-west-1"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = ["eu-west-1a", "eu-west-1b", ]
+  azs      = ["us-east-1a", "us-east-1b", ]
 
   public_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
   private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
@@ -12,7 +12,4 @@ locals {
     Name         = local.name
     CreationDate = "${formatdate("YYYY-MM-DD", timestamp())}"
   }
-
-  instance_types = ["t2.micro"]
-  ami_type       = "ami-09b024e886d7bbe74"
 }
